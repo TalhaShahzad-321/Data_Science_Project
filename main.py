ef0e008d1b4d4ce216d08cbbfd1b320c86fc163d
TalhaShahzad-321 <shahzadtalha843@gmail.com>
2025-10-19 16:47:27 +0500

    Add Data ingestion Code

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from src.datascience import logger
-logger.info("Welcome to our custom data science logs")
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
