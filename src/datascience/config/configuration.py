2da3d2d3f89dde7754b905c9883204abf7586442
TalhaShahzad-321 <shahzadtalha843@gmail.com>
2025-10-21 01:24:33 +0500

    Model Evaluation is completed

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,6 +1,10 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
-from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
+
+from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,
+                                                  DataTransformationConfig,ModelTrainerConfig,
+                                                  ModelEvaluationConfig)
+
 class ConfigurationManager:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
@@ -25,7 +29,7 @@ class ConfigurationManager:
 
         )
         return data_ingestion_config
-
+    
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
@@ -42,16 +46,14 @@ class ConfigurationManager:
         return data_validation_config
     
     def get_data_transformation_config(self) -> DataTransformationConfig:
-        config = self.config.data_transformation
-
+        config=self.config.data_transformation
         create_directories([config.root_dir])
-
-        data_transformation_config = DataTransformationConfig(
+        data_transformation_config=DataTransformationConfig(
             root_dir=config.root_dir,
-            data_path=config.data_path,
+            data_path=config.data_path
         )
-
         return data_transformation_config
+    
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.ElasticNet
@@ -71,3 +73,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/krishnaik06/datascienceproject.mlflow"
+
+
+        )
+        return model_evaluation_config
